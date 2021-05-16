457e7b1c2d31c6214f50e11ad6d43e3618b9e334
ccingram94 <constanceingram94@gmail.com>
2021-05-16 16:07:48 -0500

    fought with Docker a little bit until Dockerfile & dockercompose started working

--- settings.py
+++ settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,5 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#app/drf_project/settings.py
 AUTH_USER_MODEL = 'movies.CustomUser'
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
